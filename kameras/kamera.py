c408c410fd483ec901c88a7417e9111d7005ab1a
Florian Timm <flotimm@gmail.com>
2023-09-22 12:23:50 +0200

    Übersichtsseite

--- kameras/kamera.py
+++ kameras/kamera.py
@@ -9,24 +9,37 @@ from time import sleep
 class Kamera(object):
     def __init__(self):
         self.cam = Picamera2()
-        still_config = self.cam.create_still_configuration()
-        self.cam.configure(still_config)
+        self.preview_config = self.cam.create_preview_configuration()
+        self.still_config = self.cam.create_still_configuration()
+        self.cam.configure(self.preview_config)
         self.cam.start()
+        #
 
-    def make_picture(self, focus) -> memoryview:
+    def make_picture(self, focus, preview=False) -> memoryview:
         data = io.BytesIO()
         print("Kamera aktiviert!")
-        if (focus == -1):
+        self.focus(focus)
+        if (preview):
+            self.cam.capture_file(data, format='jpeg')
+        else:
+            req = self.cam.switch_mode_and_capture_image(
+                self.still_config)
+            req.save(data, format='jpeg')
+        print("Bild gemacht!")
+        data.seek(0)
+        return data.read()
+
+    def focus(self, focus):
+        if (focus == -2):
+            pass
+        elif (focus == -1):
             self.cam.set_controls({"AfMode": controls.AfModeEnum.Auto})
             self.cam.autofocus_cycle()
         else:
             self.cam.set_controls(
                 {"AfMode": controls.AfModeEnum.Manual, "LensPosition": focus})
             sleep(0.5)
-        self.cam.capture_file(data, format='jpeg')
-        print("Bild gemacht!")
-        data.seek(0)
-        return data.read()
+        return "Fokus"
 
     def get_status(self):
         return self.cam.camera_properties
--- kameras/kameraSteuerung.py
+++ kameras/kameraSteuerung.py
@@ -6,16 +6,14 @@
 @version: 2023.09.21
 """
 
-from flask import Flask, request, send_file, Response, make_response
+from flask import Flask, make_response
 from threading import Thread
 import configparser
 from os import system
 from sys import exit
 from kamera import Kamera
-
-
-import io
-from threading import Condition
+import socket
+from time import sleep
 
 
 class KameraSteuerung:
@@ -33,6 +31,8 @@ class KameraSteuerung:
         # load config file
         self.__conf = configparser.ConfigParser()
         self.__conf.read("config.ini")
+        t = Thread(target=self.broadcast)
+        t.start()
 
     def shutdown(self):
         """ Shutdown Raspberry Pi """
@@ -46,6 +46,22 @@ class KameraSteuerung:
 
     def photo(self, focus):
         return self.cam.make_picture(focus)
+
+    def preview(self, focus):
+        return self.cam.make_picture(focus, preview=True)
+
+    def focus(self, focus):
+        return self.cam.focus(focus)
+
+    def broadcast(self):
+        msg = b'hello world'
+        while True:
+
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as sock:
+                sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+                sock.sendto(msg, ("255.255.255.255", 5005))
+
+            sleep(2)
 
 
 # web control
@@ -61,7 +77,23 @@ def web_index():
         <meta name="viewport" content="width=device-width; initial-scale=1.0;" />
     </head>
     <body>
-    <a href="./photo"><img src="photo" width="853" height="480" /></a>
+    <script>
+        window.onload = function() {
+            let image = document.getElementById("img");
+
+            function updateImage() {
+                image.src = "preview/-2?" + new Date().getTime();
+            }
+            setInterval(updateImage, 1000);
+
+            let focus = document.getElementById("focus");
+            focus.onclick= function() {
+                fetch("focus/-1")
+            }
+        }   
+    </script>
+    <a href="./photo"><img id="img" src="preview/-2" width="640" height="480" /></a>
+    <br /><button id="focus">Autofokus</button>
     </body>
     </html>"""
 
@@ -92,6 +124,23 @@ def photo(focus=-1):
     response = make_response(stream)
     response.headers.set('Content-Type', 'image/jpeg')
     return response
+
+
+@app.route('/preview/')
+@app.route('/preview/<focus>')
+def preview(focus=-2):
+    focus = float(focus)
+    stream = ks.preview(focus)
+    response = make_response(stream)
+    response.headers.set('Content-Type', 'image/jpeg')
+    return response
+
+
+@app.route('/focus/')
+@app.route('/focus/<focus>')
+def focus(focus=-1):
+    focus = float(focus)
+    return ks.focus(focus)
 
 
 def start_web():
