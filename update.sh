100d9abc92836974623d10381e3530c4914e102c
Florian Timm <FlorianTimm@github.com>
2023-11-09 15:40:54 +0100

    UpdateService

--- update.sh
+++ update.sh
@@ -6,12 +6,24 @@ cd /home/photo/PhotoBox/
 # Git aktualisieren
 git pull
 
-cp  /home/photo/PhotoBox/PhotoBoxMaster.service /etc/systemd/system/PhotoBoxMaster.service
-cp  /home/photo/PhotoBox/PhotoBoxKamera.service /etc/systemd/system/PhotoBoxKamera.service
+cp  /home/photo/PhotoBox/PhotoBoxUpdate.service /etc/systemd/system/PhotoBoxMaster.service
 systemctl daemon-reload
+systemctl enable PhotoBoxUpdate.service || /bin/true
 
-# Service neu starten
-systemctl restart PhotoBoxMaster.service || /bin/true
-systemctl restart PhotoBoxKamera.service || /bin/true
+FILE=/etc/systemd/system/PhotoBoxMaster.service
+if test -f "$FILE"; then
+    cp  /home/photo/PhotoBox/master/PhotoBoxMaster.service /etc/systemd/system/PhotoBoxMaster.service
+    systemctl daemon-reload
+    systemctl enable PhotoBoxMaster.service || /bin/true
+    systemctl restart PhotoBoxMaster.service || /bin/true
+fi
+
+FILE=/etc/systemd/system/PhotoBoxMaster.service
+if test -f "$FILE"; then
+    cp  /home/photo/PhotoBox/kameras/PhotoBoxKamera.service /etc/systemd/system/PhotoBoxKamera.service
+    systemctl daemon-reload
+    systemctl restart PhotoBoxKamera.service || /bin/true
+fi
+
 
 
