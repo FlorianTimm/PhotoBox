de18ba29b9437fa2fb1ecfacb69b8a5a116af6cb
Florian Timm <FlorianTimm@github.com>
2023-11-09 16:13:11 +0100

    -

--- update.sh
+++ update.sh
@@ -6,12 +6,14 @@ cd /home/photo/PhotoBox/
 # Git aktualisieren
 git pull
 
+rm /etc/systemd/system/PhotoBoxUpdate.service
 cp  /home/photo/PhotoBox/PhotoBoxUpdate.service /etc/systemd/system/PhotoBoxUpdate.service
 systemctl daemon-reload
 systemctl enable PhotoBoxUpdate.service || /bin/true
 
 FILE=/etc/systemd/system/PhotoBoxMaster.service
 if test -f "$FILE"; then
+    rm /etc/systemd/system/PhotoBoxMaster.service
     cp  /home/photo/PhotoBox/master/PhotoBoxMaster.service /etc/systemd/system/PhotoBoxMaster.service
     systemctl daemon-reload
     systemctl enable PhotoBoxMaster.service || /bin/true
@@ -20,6 +22,7 @@ fi
 
 FILE=/etc/systemd/system/PhotoBoxMaster.service
 if test -f "$FILE"; then
+    rm /etc/systemd/system/PhotoBoxKamera.service
     cp  /home/photo/PhotoBox/kameras/PhotoBoxKamera.service /etc/systemd/system/PhotoBoxKamera.service
     systemctl daemon-reload
     systemctl restart PhotoBoxKamera.service || /bin/true
