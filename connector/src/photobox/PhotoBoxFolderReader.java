3b7a0e92e08c026f9b837853a63039bd720978fd
Florian Timm <FlorianTimm@github.com>
2024-03-18 14:29:12 +0100

    Agisoft Metashape Verbindung

---
+++ connector/src/photobox/PhotoBoxFolderReader.java
@@ -0,0 +1,174 @@
+package photobox;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import photobox.domain.PbCamera;
+import photobox.domain.PbImage;
+import photobox.domain.PbMarker;
+import photobox.domain.PbMarkerPosition;
+
+public class PhotoBoxFolderReader {
+
+    private Connector connector;
+    private String projectFolder;
+
+    private ArrayList<PbCamera> cameras = new ArrayList<PbCamera>();
+    private ArrayList<PbMarker> markers = new ArrayList<PbMarker>();
+    private ArrayList<PbImage> images = new ArrayList<PbImage>();
+
+    public PhotoBoxFolderReader(Connector connector, String projectFolder) {
+        this.connector = connector;
+        this.projectFolder = projectFolder;
+
+        this.cameras = new ArrayList<PbCamera>();
+        this.markers = new ArrayList<PbMarker>();
+        this.images = new ArrayList<PbImage>();
+
+        this.readPhotos();
+        this.readMarkerCoordinates();
+        this.readMarkerPositions();
+    }
+
+    private PbCamera getCameraByName(String hostname) {
+        for (PbCamera camera : this.cameras) {
+            if (camera.getCameraName().equals(hostname)) {
+                return camera;
+            }
+        }
+        return null;
+    }
+
+    private void readPhotos() {
+        this.connector.log("Adding photos");
+        try {
+            File[] files = new File(this.projectFolder).listFiles();
+            ArrayList<String> photoPathList = new ArrayList<String>();
+            for (File file : files) {
+                if (file.isFile() && file.getName().toLowerCase().endsWith(".jpg")) {
+                    photoPathList.add(file.getAbsolutePath());
+
+                    String filename = file.getName();
+                    int index = filename.lastIndexOf(".");
+                    int index_ = filename.lastIndexOf("_");
+                    if (index_ > 0) {
+                        index = index_;
+                    }
+                    String hostname = filename.substring(0, index);
+
+                    PbCamera camera = this.getCameraByName(hostname);
+                    if (camera == null) {
+                        camera = new PbCamera(hostname);
+                        this.cameras.add(camera);
+                    }
+                    PbImage image = new PbImage(camera, file);
+                    camera.addImage(image);
+                    this.images.add(image);
+                }
+            }
+        } catch (Exception e) {
+            this.connector.log(e.getMessage());
+            e.printStackTrace();
+            throw new RuntimeException("Failed to add photos");
+        }
+    }
+
+    private void readMarkerCoordinates() {
+
+        try {
+            // Read the JSON file
+            String jsonFilePath = this.projectFolder + "/marker.json";
+            String jsonContent = new String(Files.readAllBytes(Paths.get(jsonFilePath)));
+
+            // Parse the JSON content
+            JSONObject markerFileJson = new JSONObject(jsonContent);
+            Iterator<String> markersJson = markerFileJson.keys();
+            while (markersJson.hasNext()) {
+                String markerId = markersJson.next();
+                JSONObject markerJson = markerFileJson.getJSONObject(markerId);
+
+                Iterator<String> edgesJson = markerJson.keys();
+                while (edgesJson.hasNext()) {
+                    String edgeId = edgesJson.next();
+                    JSONArray edgeJson = markerJson.getJSONArray(edgeId);
+
+                    double x = edgeJson.getDouble(0);
+                    double y = edgeJson.getDouble(1);
+                    double z = edgeJson.getDouble(2);
+
+                    this.markers.add(new PbMarker(Integer.parseInt(markerId), Integer.parseInt(edgeId), x, y, z));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to add marker coordinates");
+        }
+    }
+
+    private PbMarker getMarker(int markerId, int markerEdgeId) {
+        for (PbMarker marker : this.markers) {
+            if (marker.getMarkerId() == markerId && marker.getMarkerEdgeId() == markerEdgeId) {
+                return marker;
+            }
+        }
+        return null;
+    }
+
+    private void readMarkerPositions() {
+
+        try {
+
+            // Read the JSON file
+            String jsonFilePath = this.projectFolder + "/aruco.json";
+            String jsonContent = new String(Files.readAllBytes(Paths.get(jsonFilePath)));
+
+            // Parse the JSON content
+            JSONObject arucoFileJson = new JSONObject(jsonContent);
+            Iterator<String> markersJson = arucoFileJson.keys();
+            while (markersJson.hasNext()) {
+                String hostname = markersJson.next();
+                JSONArray markerJson = arucoFileJson.getJSONArray(hostname);
+                for (int i = 0; i < markerJson.length(); i++) {
+                    JSONObject jsonMarker = markerJson.getJSONObject(i);
+                    int markerId = jsonMarker.getInt("id");
+                    int markerCorner = jsonMarker.getInt("corner");
+                    double x = jsonMarker.getDouble("x");
+                    double y = jsonMarker.getDouble("y");
+
+                    PbCamera camera = this.getCameraByName(hostname);
+                    PbImage image = camera.getImages()[0];
+                    PbMarker marker = this.getMarker(markerId, markerCorner);
+                    if (marker == null) {
+                        marker = new PbMarker();
+                    }
+
+                    PbMarkerPosition markerPosition = new PbMarkerPosition(marker, image, x, y);
+                    marker.addMarkerPosition(markerPosition);
+                }
+
+            }
+        } catch (Exception e) {
+            this.connector.log(e.getMessage());
+            e.printStackTrace();
+            throw new RuntimeException("Failed to add marker positions");
+        }
+    }
+
+    public PbCamera[] getCameras() {
+        return this.cameras.toArray(new PbCamera[this.cameras.size()]);
+    }
+
+    public PbMarker[] getMarkers() {
+        return this.markers.toArray(new PbMarker[this.markers.size()]);
+    }
+
+    public PbImage[] getImages() {
+        return this.images.toArray(new PbImage[this.images.size()]);
+    }
+}
---
+++ connector/src/photobox/metashape/MetashapeClient.java
@@ -0,0 +1,89 @@
+package photobox.metashape;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import com.agisoft.metashape.License;
+
+import photobox.Connector;
+import photobox.SfmClient;
+
+public class MetashapeClient implements SfmClient {
+
+    private final Connector connector;
+
+    public MetashapeClient(Connector connector) {
+        this.connector = connector;
+    }
+
+    public boolean connect() {
+        if (!loadLibrary()) {
+            connector.log("Failed to load library");
+            return false;
+        }
+
+        if (!testLicense()) {
+            connector.log("Failed to validate license");
+            return false;
+        }
+        connector.log("Connected to Metashape");
+        return true;
+    }
+
+    public boolean disconnect() {
+        connector.log("Disconnected from Metashape");
+        return true;
+    }
+
+    private boolean loadLibrary() {
+        ArrayList<Error> errors = new ArrayList<Error>();
+        try {
+            System.loadLibrary("metashape");
+            return true;
+        } catch (UnsatisfiedLinkError e) {
+            errors.add(e);
+        }
+
+        String currentDirectory = System.getProperty("user.dir");
+        String os = System.getProperty("os.name").toLowerCase();
+        String arch = System.getProperty("os.arch").toLowerCase();
+        String path = "";
+        if (os.contains("win") && arch.contains("64")) {
+            path = "/lib/jniLibs/win64/metashape.dll";
+        } else if (os.contains("win") && arch.contains("32")) {
+            path = "/lib/jniLibs/win32/metashape.dll";
+        } else if (os.toLowerCase().contains("mac")) {
+            path = "/lib/jniLibs/macosx64/libmetashape.dylib";
+        } else if (os.toLowerCase().contains("nix") || os.toLowerCase().contains("nux")) {
+            path = "/lib/jniLibs/linux64/libmetashape.so";
+        }
+
+        try {
+            System.load(currentDirectory + path);
+            return true;
+        } catch (UnsatisfiedLinkError e) {
+            errors.add(e);
+        }
+
+        for (Error e : errors) {
+            connector.log(e.getMessage());
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private boolean testLicense() {
+        if (License.isValid()) {
+            return true;
+        }
+        JOptionPane.showMessageDialog(null, "License is invalid");
+        return false;
+    }
+
+    @Override
+    public void processPhotos(String destDir) {
+        connector.log("Processing photos");
+        MetashapeProject project = new MetashapeProject(connector, destDir);
+        project.run();
+    }
+
+}
