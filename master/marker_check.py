57a4d1aff50cf5e85efa568edd5498f285d8e7d3
Florian Timm <FlorianTimm@github.com>
2024-04-04 12:55:25 +0200

    Test

--- master/marker_check.py
+++ master/marker_check.py
@@ -141,6 +141,7 @@ class MarkerChecker:
         t = self.__marker_pos.groupby(['id', 'corner'])['inlier'].agg(
             [pd.Series.count, pd.Series.mode])
         # Replace with True if mode is a list (Number of outliers = Number of inliers)
+        Logger().info(type(t['mode']))
         t['mode'] = t['mode'].apply(
             lambda x: True if isinstance(x, list) else x)
 
